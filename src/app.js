4933c3454b3bb670efad2e3c9fbb56ce79dd0d86
Marc Miró <marc.miro@decathlon.com>
2021-06-23 21:17:37 +0200

    fix: change api port to 8080

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 // Express Initialize
 const app = express();
-const port = 3000;
+const port = 8080;
 
 app.listen(port, () => console.log(`App is up using port ${port}!`));
 
